bba353dcce2a45fa4c7367b2396d6c3fb833eea1
Benton Westergaard <benton.westergaard@gmail.com>
2020-11-18 22:03:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,8 +1,7 @@
-def loop_message_five_times(message)
-  count = 0
-  while count < 5 do
-    puts message
-    count += 1
-  end
-
-  
+    def loop_message_five_times(message)
+      count = 0
+      while count < 5 do
+        puts message
+        count += 1
+      end
+    end
